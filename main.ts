b286073b34b422f39852203bc4e7405c0819a970
Carlos "Café Café" García <58571285+Carlos-CafeCafe@users.noreply.github.com>
2023-07-13 18:51:17 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == partitura[indicePartitura]) {
+function seleccionarMúsica () {
+    if (musica == 1) {
+        partitura = Para_Elisa
+    }
+    if (musica == 2) {
+        partitura = Feliz_Cumpleaños
+    }
+    indicePartitura = 0
+    basic.showString("" + (partitura[indicePartitura]))
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    musica = receivedNumber
+    seleccionarMúsica()
+})
+function evaluarNota () {
+    if (notaRecibida == partitura[indicePartitura]) {
         basic.showIcon(IconNames.Yes)
         indicePartitura += 1
         if (indicePartitura == partitura.length) {
@@ -11,16 +25,29 @@ radio.onReceivedString(function (receivedString) {
         basic.showIcon(IconNames.No)
         basic.showString("" + (partitura[indicePartitura]))
     }
+}
+radio.onReceivedString(function (receivedString) {
+    notaRecibida = receivedString
+    evaluarNota()
 })
+let notaRecibida = ""
 let indicePartitura = 0
 let partitura: string[] = []
-radio.setGroup(1)
-partitura = [
+let musica = 0
+let Feliz_Cumpleaños: string[] = []
+let Para_Elisa: string[] = []
+Para_Elisa = [
+"c",
+"a",
+"b",
+"b",
+"b"
+]
+Feliz_Cumpleaños = [
 "a",
 "b",
 "c",
 "a",
 "b"
 ]
-indicePartitura = 0
-basic.showString("" + (partitura[indicePartitura]))
+radio.setGroup(1)
